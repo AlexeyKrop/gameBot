683aa6b6177ff1d8528b5eac3520d4760d02fcfb
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-08 21:44:46 +0300

    Переделал задание

--- script.js
+++ script.js
@@ -1,49 +1,35 @@
 'use strict';
-let n = 10;
-function gameBot(){
-  function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  let randomNumber = getRandom(1, 100);
+
+let randomNumberResult;
+
+function gameBot(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   console.log(randomNumber);
-  function getNumber(){
+  return function () {
     let userEnterNumber = prompt('Угадай число от 1 до 100');
-    if(userEnterNumber === null){
-      alert('Игра окончена');
-    } else if(isNaN(userEnterNumber) || userEnterNumber.trim() === ''){
+      if(userEnterNumber === null){
+        alert('Игра окончена');
+      } else if(isNaN(userEnterNumber) || userEnterNumber.trim() === '') {
         alert('Введи число');
-        getNumber();
-    } else if (+userEnterNumber === randomNumber){
-        let newGame = confirm(`Поздравляю, Вы угадали!!! Хотели бы сыграть еще?`); 
-        if(newGame === true) {
-          n = 10;
-          getNumber();
-        } else {
-          alert('Пока');
-        }
-    } else if (+userEnterNumber > 100) {
-        alert(`Загаданное число меньше, осталось попыток ${--n}`);
-        if(n < 1){
-          alert('Игра закончена');
-        }else{
-          getNumber();
-        }
-    } else if(+userEnterNumber < 1){
-        alert(`Загаданное число больше, осталось попыток ${--n}`);
-        if(n < 1){
-          alert('Игра закончена');
-        }else{
-          getNumber();
-        }
-    } else if(+userEnterNumber !== randomNumber){
+        randomNumberResult();
+      } else if(+userEnterNumber > 100){
+        alert('Загаданное число меньше');
+        randomNumberResult();
+      } else if(+userEnterNumber < 1){
+        alert('Загаданное число больше');
+        randomNumberResult();
+      } else if(+userEnterNumber === randomNumber){
+        alert(`Поздравляю, Вы угадали!!!`); 
+      } else if(+userEnterNumber !== randomNumber){
       alert(`Вы проиграли(((`);
     }
-    }
-    getNumber();
+  };
 }
-gameBot();
+// Тут ошибку выдает, потому что мы используем переменную, до ее объявления, но как иначе вызвать функцию?
+randomNumberResult = gameBot(1,100);
+randomNumberResult();
  
 
 
