f4b27fff581e3acf6ef5b68ad0e542cafdb8f775
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-09 18:51:01 +0300

    Исправил замечания

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
 'use strict';
-let n = 10;
-function gameBot(){
-  function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  let randomNumber = getRandom(1, 100);
+let randomNumberResult;
+
+
+function gameBot(min,max) {
+  let n = 10;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   console.log(randomNumber);
-  function getNumber(){
+  function getNumberOfAttempts() {
+    if(n < 1){
+      alert('Игра закончена');
+    } else {
+      randomNumberResult();
+      }
+  }
+  function MyFunc() {
     let userEnterNumber = prompt('Угадай число от 1 до 100');
-    if(userEnterNumber === null){
-      alert('Игра окончена');
-    } else if(isNaN(userEnterNumber) || userEnterNumber.trim() === ''){
+      if(userEnterNumber === null){
+        alert('Игра окончена');
+      } else if(isNaN(userEnterNumber) || userEnterNumber.trim() === '') {
         alert('Введи число');
-        getNumber();
-    } else if (+userEnterNumber === randomNumber){
-        let newGame = confirm(`Поздравляю, Вы угадали!!! Хотели бы сыграть еще?`); 
-        if(newGame === true) {
-          n = 10;
-          getNumber();
-        } else {
-          alert('Пока');
-        }
-    } else if (+userEnterNumber > 100) {
-        alert(`Загаданное число меньше, осталось попыток ${--n}`);
-        if(n < 1){
-          alert('Игра закончена');
-        }else{
-          getNumber();
-        }
-    } else if(+userEnterNumber < 1){
-        alert(`Загаданное число больше, осталось попыток ${--n}`);
-        if(n < 1){
-          alert('Игра закончена');
-        }else{
-          getNumber();
-        }
-    } else if(+userEnterNumber !== randomNumber){
-      alert(`Вы проиграли(((`);
+        randomNumberResult();
+      } else if(+userEnterNumber === randomNumber){
+          let newGame = confirm(`Поздравляю, Вы угадали!!! Хотели бы сыграть еще?`); 
+          if (newGame === true) {
+            randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            console.log(randomNumber);
+            n = 10;
+            randomNumberResult();
+          } else {
+            alert('Приходи еще');
+          }
+      } else if(+userEnterNumber > 100){
+          alert(`Загаданное число меньше, осталось попыток ${--n}`);
+          getNumberOfAttempts();
+      } else if(+userEnterNumber < 1){
+          alert(`Загаданное число меньше, осталось попыток ${--n}`);
+          getNumberOfAttempts();
+      } else if(+userEnterNumber !== randomNumber){
+      alert(`Осталось попыток ${--n}`);
+      getNumberOfAttempts();
     }
-    }
-    getNumber();
-}
-gameBot();
+    gameBot(1,100);
+  }
+  return MyFunc;
+} 
+randomNumberResult = gameBot(1,100);
+randomNumberResult();
  
 
 
